b33b2d37ce242aa2518a7a4ba5d1d3616daa0d07
PhamVanDong <phamdong9139@gmail.com>
2024-03-15 17:25:59 +0700

    init poject

---
+++ Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+    [Header("Shooting")]
+    [SerializeField] AudioClip shootingClip;
+    [SerializeField][Range(0f, 1f)] float shootingVolume = 1f;
+
+    [Header("Damage")]
+    [SerializeField] AudioClip damageClip;
+    [SerializeField][Range(0f, 1f)] float damageVolume = 1f;
+
+    [Header("Health")]
+    [SerializeField] AudioClip healthClip;
+    [SerializeField][Range(0f, 1f)] float healthVolume = 1f;
+
+    static AudioPlayer instance;
+
+    private void Awake()
+    {
+        ManageSingleton();
+    }
+
+    void ManageSingleton()
+    {
+        if(instance != null)
+        {
+            gameObject.SetActive(false);
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    public void PlayShootingClip()
+    {
+        PlayClip(shootingClip, shootingVolume);
+    }
+
+    public void PlayDamageClip()
+    {
+        PlayClip(damageClip, damageVolume);
+    }
+
+    public void PlayHealthClip()
+    {
+        PlayClip(healthClip, healthVolume);
+    }
+    void PlayClip(AudioClip clip, float volume)
+    {
+        if(clip != null)
+        {
+            Vector3 cameraPos = Camera.main.transform.position;
+            AudioSource.PlayClipAtPoint(clip, cameraPos, volume);
+        }
+    }
+
+}
---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+    [SerializeField] List<WaveConfigSO> waveConfigs;
+    [SerializeField] float timeBetweenWaves;
+    WaveConfigSO currentWave;
+
+    [SerializeField] bool isLooping;
+    /// <summary>
+    /// 
+    /// </summary>
+    [SerializeField] GameObject bossPref;
+    [SerializeField] bool isSpawn = true;
+    [SerializeField] int n_score = 500;
+    Scorekeeper scorekeeper;
+
+    private void Awake()
+    {
+        scorekeeper = FindObjectOfType<Scorekeeper>();
+    }
+    /// <summary>
+    /// //
+    /// </summary>
+    void Start()
+    {
+        StartCoroutine(SpawnEnemyWave());
+    }
+
+    private void Update()
+    {
+        if (scorekeeper.GetScore() >= n_score && isSpawn)
+        {
+            isLooping = false;
+            //spawn boss
+            SpawnBoss();
+            isSpawn = false;
+        }
+    }
+
+    public WaveConfigSO GetCurrentWave()
+    {
+        return currentWave;
+    }
+
+    IEnumerator SpawnEnemyWave()
+    {
+        do
+        {
+            foreach (WaveConfigSO wave in waveConfigs)
+            {
+                currentWave = wave;
+                for (int i = 0; i < currentWave.GetEnemyCount(); i++)
+                {
+                    Instantiate(currentWave.GetEnemyPrefab(i),
+                            currentWave.GetStartingWaypoint().position,
+                            Quaternion.Euler(0,0,180),
+                            transform);
+                    yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
+                }
+                yield return new WaitForSeconds(timeBetweenWaves);
+            }
+        }
+        while (isLooping);
+    }
+
+    void SpawnBoss()
+    {
+        Vector3 spawnPos = new Vector3(transform.position.x, transform.position.y + 12);
+        GameObject bossClone = Instantiate(bossPref, spawnPos, Quaternion.identity, transform);
+    }
+}
